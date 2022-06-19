291c5290012539e2ce99c66e1819d89ef4ba9808
Sridhar S <34412034+Sri-Sridhar@users.noreply.github.com>
2022-06-19 11:29:13 +0530

    delete msg update

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -48,7 +48,7 @@ export class MainSectionComponent implements OnInit {
     snackRef.onAction().subscribe(() => {
       console.log('undo clicked');
       // el.style.display = 'block';
-      this._snackbar.open('hehe','',{ duration: 1000 });
+      this._snackbar.open('feature is limited to pro users 🥰','',{ duration: 1000 });
     });
 
     if (this.doneTodoList.includes(todo) && todo.isDone == true) {
